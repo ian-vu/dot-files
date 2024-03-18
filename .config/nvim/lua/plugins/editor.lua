a613e6a1e5c8fae2360fde8c31b02062d2f88433
Ian Vu <iaan.vuu@gmail.com>
2024-03-18 23:43:27 +1100

    Add chatgpt to nvim

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -132,4 +132,18 @@ return {
       },
     },
   },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "op read op://kt76oi5s3tqjg54lvlolplvvaq/ywcqft6be3aw45ic3wirvts5su/password",
+      })
+    end,
+  },
 }
