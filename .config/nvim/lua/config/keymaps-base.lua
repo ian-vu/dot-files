d3f07ac14ce4ad2e4af5bcb9fb5c399a2dfa226e
Ian Vu <iaan.vuu@gmail.com>
2024-03-31 18:54:29 +1100

    nvim: add keymap show explorer without focus

--- .config/nvim/lua/config/keymaps-base.lua
+++ .config/nvim/lua/config/keymaps-base.lua
@@ -99,6 +99,9 @@ vim.keymap.set({ "n", "v" }, "<C-_>", lazyterm, { desc = "Terminal (root dir)" }
 
 -- Neotree Plugin
 vim.keymap.set({ "n", "v" }, "<C-n>", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neotree", remap = true })
+vim.keymap.set({ "n", "v" }, "<leader>es", function()
+  vim.api.nvim_command("Neotree action=show position=left filesystem")
+end)
 vim.keymap.set({ "n", "v" }, "<leader>ee", function()
   -- local Config = require("edgy.config")
   -- for p, edgebar in pairs(Config.layout) do
