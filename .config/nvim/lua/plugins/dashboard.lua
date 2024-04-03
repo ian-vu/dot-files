f301d25c47fdc9576319cc87165cf5c1d1dc7b88
Ian Vu <iaan.vuu@gmail.com>
2024-04-03 22:28:47 +1100

    nvim: add dashboard

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -3,17 +3,36 @@ return {
     "nvimdev/dashboard-nvim",
     event = "VimEnter",
     opts = function()
+      -- local logo = [[
+      --      ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+      --      ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z
+      --      ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z
+      --      ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z
+      --      ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║
+      --      ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝
+      -- ]]
       local logo = [[
-           ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-           ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
-           ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
-           ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
-           ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
-           ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
+ ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓
+ ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒
+▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░
+▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██ 
+▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒
+░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░
+░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░
+   ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░   
+         ░    ░  ░    ░ ░        ░   ░         ░   
+                                ░                  
       ]]
+      --      local logo = [[
+      -- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+      -- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+      -- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+      -- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+      -- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+      -- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+      --      ]]
 
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-
+      logo = string.rep("\n", 8) .. logo .. string.rep("\n", 3)
       local opts = {
         theme = "doom",
         hide = {
@@ -25,22 +44,25 @@ return {
           header = vim.split(logo, "\n"),
           -- stylua: ignore
           center = {
-            { action = "ene | startinsert",                 desc = " New file",        icon = " ", key = "n" },
-            { action = 'Neotree position=float',            desc = " Explorer",        icon = "󱏒 ", key = "e" },
-            { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s" },
-            { action = "qa",                                desc = " Quit",            icon = " ", key = "q" },
+            { action = 'lua require("persistence").load()', desc = " Restore Session", icon = " ", key = "s"},
+            { action = 'Neotree position=float',            desc = " Explorer",        icon = "󱏒 ", key = "e"},
+            { action = "ene | startinsert",                 desc = " New file",        icon = " ", key = "n"},
+            { action = "qa",                                desc = " Quit",            icon = " ", key = "q"},
           },
-          footer = function()
-            local stats = require("lazy").stats()
-            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-          end,
+          footer = {},
+          -- Show Neovim metadata
+          -- footer = function()
+          --   local stats = require("lazy").stats()
+          --   local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+          --   return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+          -- end,
         },
       }
 
+      -- Set format
       for _, button in ipairs(opts.config.center) do
-        button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-        button.key_format = "  %s"
+        button.desc = button.desc .. string.rep(" ", 41 - #button.desc)
+        button.key_format = "{ %s }"
       end
 
       -- close Lazy and re-open when the dashboard is ready
