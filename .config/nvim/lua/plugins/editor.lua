2c2dd39d82bbfc4b2a76ef099562e0a7f80c6a66
Ian Vu <iaanvuu@gmail.com>
2024-04-10 22:03:56 +1000

    nvim: add commented out vim-visual-multi as requires too much to work with colemak bindings

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -212,4 +212,14 @@ return {
       },
     },
   },
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   branch = "master",
+  --   init = function()
+  --     vim.g.VM_maps = {
+  --       ["Find Under"] = "<C-S-d>",
+  --       ["Find Subword Under"] = "<C-S-d>",
+  --     }
+  --   end,
+  -- },
 }
