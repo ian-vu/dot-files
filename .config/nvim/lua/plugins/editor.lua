255c0498ddc64c0ca3f962aae9375ca589a1b5ac
Ian Vu <iaanvuu@gmail.com>
2024-04-11 22:22:57 +1000

    nvim: move telescope to own plugin file

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,48 +1,5 @@
 return {
   {
-    "nvim-telescope/telescope.nvim",
-    keys = {
-      { "<leader>/", false },
-    },
-    opts = {
-      defaults = {
-        layout_config = {
-          prompt_position = "top",
-          vertical = { width = 0.1 },
-          horizontal = { preview_width = 0.5 },
-        },
-        results_title = false,
-        path_display = { "truncate" }, -- Clip the path if too long, always showing the file name
-        dynamic_preview_title = true, -- Show title of file in preview window at top
-        scroll_strategy = "limit", -- Don't cycle to top of list when reaching bottom
-        sorting_strategy = "ascending",
-        wrap_results = false,
-        hl_result_eol = false,
-        mappings = {
-          i = {
-            ["<esc>"] = require("telescope.actions").close,
-            ["<C-j>"] = require("telescope.actions").cycle_history_next,
-            ["<C-n>"] = require("telescope.actions").cycle_history_next,
-            ["<C-k>"] = require("telescope.actions").cycle_history_prev,
-            ["<C-e>"] = require("telescope.actions").cycle_history_prev,
-            ["<D-j>"] = require("telescope.actions").cycle_history_next,
-            ["<D-n>"] = require("telescope.actions").cycle_history_next,
-            ["<D-k>"] = require("telescope.actions").cycle_history_prev,
-            ["<D-e>"] = require("telescope.actions").cycle_history_prev,
-          },
-        },
-      },
-    },
-  },
-  {
-    -- Allow for telescope plugin that groups results by files
-    "fdschmidt93/telescope-egrepify.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").load_extension("egrepify")
-    end,
-  },
-  {
     -- Show underline of current words under cursor
     "RRethy/vim-illuminate",
     event = "LazyFile",
