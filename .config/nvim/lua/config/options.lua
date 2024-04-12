5e5c46b7cc10260566b658d89c6098cbdc4bcf19
Ian Vu <iaanvuu@gmail.com>
2024-04-12 15:21:27 +1000

    nvim: increase scrolloff

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -21,7 +21,7 @@ vim.opt.titlestring = get_title()
 vim.opt.conceallevel = 0
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 15
 
 -- Always show sign column since when going to and from insert mode shifts text
 vim.opt.signcolumn = "yes"
