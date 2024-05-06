32654040aacc5dd1a8b3a7a181dd8b65b774a56d
Ian Vu <iaanvuu@gmail.com>
2024-05-06 15:23:34 +1000

    nvim: telescope to show files name then path after

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,26 +4,60 @@ return {
     keys = {
       { "<leader>/", false },
     },
-    opts = {
-      pickers = {
+    opts = function(_, opts)
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "TelescopeResults",
+        callback = function(ctx)
+          vim.api.nvim_buf_call(ctx.buf, function()
+            vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+            vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+          end)
+        end,
+      })
+
+      local function filenameFirst(_, path)
+        local tail = vim.fs.basename(path)
+        local parent = vim.fs.dirname(path)
+        if parent == "." then
+          return tail
+        end
+        return string.format("%s\t\t%s", tail, parent)
+      end
+
+      FILE_IGNORE_PATTERNS = {
+        ".git",
+        "node_modules",
+        "venv",
+        ".venv",
+        "__pycache__",
+        ".mypy_cache",
+        ".pytest_cache",
+        ".idea",
+      }
+
+      opts.pickers = vim.tbl_deep_extend("force", opts.pickers or {}, {
         find_files = {
           hidden = true,
+          no_ignore = true,
+          file_ignore_patterns = FILE_IGNORE_PATTERNS,
         },
         live_grep = {
-          file_ignore_patterns = { ".git", "node_modules", "venv", ".venv" },
+          file_ignore_patterns = FILE_IGNORE_PATTERNS,
           additional_args = function(_)
             return { "--hidden" }
           end,
         },
-      },
-      defaults = {
+      })
+
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
         layout_config = {
           prompt_position = "top",
           vertical = { width = 0.1 },
           horizontal = { preview_width = 0.5 },
         },
         results_title = false,
-        path_display = { "truncate" }, -- Clip the path if too long, always showing the file name
+        -- path_display = { "truncate" }, -- Clip the path if too long, always showing the file name
+        path_display = filenameFirst,
         dynamic_preview_title = true, -- Show title of file in preview window at top
         -- Uncommenting the below causes issues with egrepify
         -- scroll_strategy = "limit", -- Don't cycle to top of list when reaching bottom
@@ -43,8 +77,8 @@ return {
             ["<D-e>"] = require("telescope.actions").cycle_history_prev,
           },
         },
-      },
-    },
+      })
+    end,
   },
   {
     -- Allow for telescope plugin that groups results by files
