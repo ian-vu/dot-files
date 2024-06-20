d66569979064f6cf54ee19201de652520483ff22
Ian Vu <iaanvuu@gmail.com>
2024-06-20 13:45:46 +1000

    nvim: add unused http client

--- .config/nvim/lua/plugins/http-client.lua
+++ .config/nvim/lua/plugins/http-client.lua
@@ -1,40 +1,40 @@
 return {
-  -- {
-  --   "vhyrro/luarocks.nvim",
-  --   priority = 1000,
-  --   config = true,
-  --   opts = {
-  --     rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-  --   },
-  -- },
-  -- {
-  --   "rest-nvim/rest.nvim",
-  --   ft = "http",
-  --   dependencies = { "luarocks.nvim" },
-  --   opts = {
-  --     result = {
-  --       split = {
-  --         horizontal = true,
-  --       },
-  --     },
-  --   },
-  --   config = function(_, opts)
-  --     require("rest-nvim").setup(opts)
-  --   end,
-  --   -- config = function(_, opts)
-  --   --   require("rest").setup(opts)
-  --   -- end,
-  --   keys = {
-  --     { "<leader>rr", "<cmd>Rest run<cr>", "Run request under cursor" },
-  --     { "<leader>rl", "<cmd>Rest run<cr>", "Run last request" },
-  --   },
-  -- },
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = function(_, opts)
-  --     if type(opts.ensure_installed) == "table" then
-  --       vim.list_extend(opts.ensure_installed, { "http" })
-  --     end
-  --   end,
-  -- },
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+    opts = {
+      rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+    },
+  },
+  {
+    "rest-nvim/rest.nvim",
+    ft = "http",
+    dependencies = { "luarocks.nvim" },
+    opts = {
+      result = {
+        split = {
+          horizontal = true,
+        },
+      },
+    },
+    config = function(_, opts)
+      require("rest-nvim").setup(opts)
+    end,
+    -- config = function(_, opts)
+    --   require("rest").setup(opts)
+    -- end,
+    keys = {
+      { "<leader>rr", "<cmd>Rest run<cr>", "Run request under cursor" },
+      { "<leader>rl", "<cmd>Rest run<cr>", "Run last request" },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "http" })
+      end
+    end,
+  },
 }
