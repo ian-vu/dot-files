27706bf9486c7c904781251ba7f2852f733da842
ian-vu <iaanvuu@gmail.com>
2024-08-27 10:16:27 +1000

    nvim: theme enable auto change

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -10,14 +10,14 @@ return {
   },
   {
     "f-person/auto-dark-mode.nvim",
-    enabled = false,
+    enabled = true,
     opts = {
       update_interval = 5000,
       set_dark_mode = function()
-        vim.api.nvim_set_option("background", "dark")
+        vim.api.nvim_set_option_value("background", "dark", { scope = "global" })
       end,
       set_light_mode = function()
-        vim.api.nvim_set_option("background", "light")
+        vim.api.nvim_set_option_value("background", "light", { scope = "global" })
       end,
     },
   },
