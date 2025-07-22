1c0a468935a0ae915403ae504eb3d649c89de196
ian-vu <iaanvuu@gmail.com>
2025-07-22 12:22:13 +1000

    nvim: add comments to easy switch to basepyright

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -1,11 +1,13 @@
 if lazyvim_docs then
   -- LSP Server to use for Python.
   -- Set to "basedpyright" to use basedpyright instead of pyright.
+  -- vim.g.lazyvim_python_lsp = "basedpyright"
   vim.g.lazyvim_python_lsp = "pyright"
   -- Set to "ruff_lsp" to use the old LSP implementation version.
   vim.g.lazyvim_python_ruff = "ruff"
 end
 
+-- local lsp = vim.g.lazyvim_python_lsp or "basedpyright"
 local lsp = vim.g.lazyvim_python_lsp or "pyright"
 local ruff = vim.g.lazyvim_python_ruff or "ruff"
 
