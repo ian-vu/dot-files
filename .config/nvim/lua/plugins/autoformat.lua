d12821989864e6f29f4129fa767a053420b68b68
ian-vu <iaanvuu@gmail.com>
2025-10-12 00:15:24 +1100

    nvim: autoformat javascript and typescript

--- .config/nvim/lua/plugins/autoformat.lua
+++ .config/nvim/lua/plugins/autoformat.lua
@@ -38,6 +38,8 @@ return {
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
 				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+				["javascript"] = { "prettierd" },
+				["typescript"] = { "prettierd" },
 				["css"] = { "prettierd" },
 				["scss"] = { "prettierd" },
 				["less"] = { "prettierd" },
