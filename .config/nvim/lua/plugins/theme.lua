8f0b19629bbc9a112a8983c00e3e2be34966c5ad
ian-vu <iaanvuu@gmail.com>
2026-03-01 21:10:07 +1100

    nvim: add gruvbox

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,39 @@
 return {
+	{
+		"sainnhe/gruvbox-material",
+		enabled = true,
+		priority = 1000,
+		config = function()
+			-- vim.cmd("set background=light")
+			-- vim.cmd("set background=dark")
+			vim.cmd("let g:gruvbox_material_background = 'hard'") -- soft | medium | hard
+			vim.cmd("let g:gruvbox_material_foreground = 'mix'") -- material | mix | original
+			-- vim.cmd("let g:gruvbox_material_transparent_background = 2")
+			-- vim.cmd("let g:gruvbox_material_better_performance = 1")
+			vim.cmd("let g:gruvbox_material_enable_bold = 0")
+			vim.cmd("let g:gruvbox_material_enable_italic = 1")
+			-- vim.cmd("let g:gruvbox_material_dim_inactive_windows = 1")
+			-- vim.cmd("let g:gruvbox_material_visual = 'blue background'")
+			vim.cmd("let g:gruvbox_material_spell_foreground = 1")
+			-- vim.cmd("let g:gruvbox_material_diagnostic_text_highlight = 1")
+			vim.cmd("let g:gruvbox_material_diagnostic_virtual_text = 'colored'") -- 'highlighted' for extra
+			-- How to highlight the current cursor word
+			vim.cmd("let g:gruvbox_material_current_word = 'grey background'") -- 'underline' or 'grey background'
+
+			-- This should be last
+			vim.cmd("colorscheme gruvbox-material")
+		end,
+		opts = {
+			inverse = true,
+		},
+	},
 	{ -- You can easily change to a different colorscheme.
 		-- Change the name of the colorscheme plugin below, and then
 		-- change the command in the config to whatever the name of that colorscheme is.
 		--
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
 		"folke/tokyonight.nvim",
+		enabled = false,
 		priority = 1000, -- Make sure to load this before all the other start plugins.
 		config = function()
 			---@diagnostic disable-next-line: missing-fields
