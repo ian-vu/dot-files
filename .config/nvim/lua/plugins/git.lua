951d9c7d3a8014ee30a8c252c921494bc4902e99
ian-vu <iaanvuu@gmail.com>
2026-03-02 21:38:04 +1100

    nvim: diffview updates and keymaps for conflict choosing

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -30,15 +30,71 @@ return {
 			local actions = require("diffview.actions")
 			return {
 				enhanced_diff_hl = true,
+				use_icons = true,
 				default_args = {
 					DiffviewOpen = { "--untracked-files=all", "--imply-local" },
 					DiffviewFileHistory = { "--base=LOCAL" },
 				},
+				show_help_hints = false,
 				keymaps = {
 					view = {
 						{ "n", "]q", actions.select_next_entry, { desc = "Open the diff for the next file" } },
 						{ "n", "[q", actions.select_prev_entry, { desc = "Open the diff for the next file" } },
 						{ "n", "<c-q>", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
+						{
+							"n",
+							"<leader>gco",
+							actions.conflict_choose("ours"),
+							{ desc = "Choose the OURS version of a conflict" },
+						},
+						{
+							"n",
+							"<leader>gct",
+							actions.conflict_choose("theirs"),
+							{ desc = "Choose the THEIRS version of a conflict" },
+						},
+						{
+							"n",
+							"<leader>gcb",
+							actions.conflict_choose("base"),
+							{ desc = "Choose the BASE version of a conflict" },
+						},
+						{
+							"n",
+							"<leader>gca",
+							actions.conflict_choose("all"),
+							{ desc = "Choose all the versions of a conflict" },
+						},
+						{
+							"n",
+							"dx",
+							actions.conflict_choose("none"),
+							{ desc = "Delete the conflict region" },
+						},
+						{
+							"n",
+							"<leader>gcO",
+							actions.conflict_choose_all("ours"),
+							{ desc = "Choose the OURS version of a conflict for the whole file" },
+						},
+						{
+							"n",
+							"<leader>gcT",
+							actions.conflict_choose_all("theirs"),
+							{ desc = "Choose the THEIRS version of a conflict for the whole file" },
+						},
+						{
+							"n",
+							"<leader>gcB",
+							actions.conflict_choose_all("base"),
+							{ desc = "Choose the BASE version of a conflict for the whole file" },
+						},
+						{
+							"n",
+							"<leader>gcA",
+							actions.conflict_choose_all("all"),
+							{ desc = "Choose all the versions of a conflict for the whole file" },
+						},
 					},
 					file_panel = {
 						{ "n", "<c-q>", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
@@ -85,6 +141,7 @@ return {
 					},
 				},
 				file_panel = {
+					listing_style = "list", -- "list" or "tree"
 					win_config = {
 						position = "bottom",
 						height = 12,
