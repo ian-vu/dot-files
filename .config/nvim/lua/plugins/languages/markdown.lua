cd90b29701c84177aa79727027756823791b0d6e
ian-vu <iaanvuu@gmail.com>
2026-04-18 18:53:11 +1000

    nvim: markdown

--- .config/nvim/lua/plugins/languages/markdown.lua
+++ .config/nvim/lua/plugins/languages/markdown.lua
@@ -41,14 +41,12 @@ return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim", -- https://github.com/MeanderingProgrammer/render-markdown.nvim
 		opts = {
-			code = {
-				sign = false,
-				width = "block",
-				right_pad = 1,
-			},
 			heading = {
 				sign = false,
 				icons = {},
+			},
+			bullet = {
+				right_pad = 1,
 			},
 		},
 		ft = { "markdown", "norg", "rmd", "org" },
